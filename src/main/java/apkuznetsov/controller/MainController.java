2acfc768c72a063ed4d1d77f9bc4fa734e8162f8
Andrey <kouznts@ya.ru>
2020-08-20 09:36:43 +0400

    impl controller get expanded form method

--- src/main/java/apkuznetsov/controller/MainController.java
+++ src/main/java/apkuznetsov/controller/MainController.java
@@ -1,18 +1,29 @@
 package apkuznetsov.controller;
 
+import apkuznetsov.solver.Solver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/",
+            method = RequestMethod.GET)
     public ModelAndView main() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
 
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/get-expanded-form",
+            method = RequestMethod.POST)
+    public String getExpandedForm(String number) {
+        int numberAsInt = Integer.parseInt(number);
+
+        return Solver.getExpandedForm(numberAsInt);
     }
 }
