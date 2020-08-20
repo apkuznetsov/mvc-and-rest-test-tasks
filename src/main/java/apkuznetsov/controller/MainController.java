b347b559334497d289e395869697e26ed1d5f065
Andrey <kouznts@ya.ru>
2020-08-20 16:59:40 +0400

    refactor main controller

--- src/main/java/apkuznetsov/controller/MainController.java
+++ src/main/java/apkuznetsov/controller/MainController.java
@@ -1,13 +1,11 @@
 package apkuznetsov.controller;
 
-import apkuznetsov.solver.Solver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
 
     @RequestMapping(value = "/",
@@ -17,13 +15,5 @@ public class MainController {
         modelAndView.setViewName("index");
 
         return modelAndView;
-    }
-
-    @RequestMapping(value = "/get-expanded-form",
-            method = RequestMethod.POST)
-    public String getExpandedForm(String number) {
-        int numberAsInt = Integer.parseInt(number);
-
-        return Solver.getExpandedForm(numberAsInt);
     }
 }
