161adc025429afaeed03c24f864d4e1e5b68f1fa
Andrey <kouznts@ya.ru>
2020-08-20 17:27:43 +0400

    impl web app config

---
+++ src/main/java/apkuznetsov/WebAppConfig.java
@@ -0,0 +1,26 @@
+package apkuznetsov;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("apkuznetsov")
+public class WebAppConfig {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
+
+}
