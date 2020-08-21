bec179b46352cec9473dbebca09ea9514ecbf5b8
Andrey <kouznts@ya.ru>
2020-08-21 20:11:41 +0400

    return expanded form number as json

---
+++ src/main/java/demo/web/model/Answer.java
@@ -0,0 +1,20 @@
+package demo.web.model;
+
+public class Answer {
+    private String answer;
+
+    public Answer() {
+    }
+
+    public Answer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
--- src/main/java/demo/web/restcontroller/SolverController.java
+++ src/main/java/demo/web/restcontroller/SolverController.java
@@ -1,7 +1,7 @@
 package demo.web.restcontroller;
 
+import demo.web.model.Answer;
 import demo.web.model.Number;
-import demo.logic.solver.Solver;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,13 +9,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static demo.logic.solver.Solver.getExpandedForm;
+
 @RestController
 @RequestMapping("/api")
 public class SolverController {
 
     @PostMapping(value = "/get-expanded-form", consumes = "application/json")
-    public ResponseEntity<String> getExpandedFormF(@RequestBody Number number) {
+    public ResponseEntity<Answer> getExpandedFormF(@RequestBody Number number) {
 
-        return new ResponseEntity<>(Solver.getExpandedForm(number.getNumber()), HttpStatus.OK);
+        return new ResponseEntity<>(new Answer(getExpandedForm(number.getNumber())), HttpStatus.OK);
     }
 }
