f3607d57749b76ddba82126274d8d85908260cd9
Andrey <kouznts@ya.ru>
2020-08-23 09:25:18 +0400

    rename task2 jsp

--- src/main/java/demo/web/controller/IndexController.java
+++ src/main/java/demo/web/controller/IndexController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class IndexController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/task2", method = RequestMethod.GET)
     public String index() {
-        return "index";
+        return "task2";
     }
 }
