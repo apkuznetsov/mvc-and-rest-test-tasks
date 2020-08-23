5c1246867bd826f1d78bb9c0efbd16b322753d0f
Andrey <kouznts@ya.ru>
2020-08-23 15:46:41 +0400

    impl substrings and strings model

---
+++ src/main/java/demo/web/model/SubstrsAndStrsInput.java
@@ -0,0 +1,26 @@
+package demo.web.model;
+
+public class SubstrsAndStrsInput {
+
+    private String[] substrings;
+    private String[] strings;
+
+    public SubstrsAndStrsInput() {
+    }
+
+    public String[] getSubstrings() {
+        return substrings;
+    }
+
+    public void setSubstrings(String[] substrings) {
+        this.substrings = substrings;
+    }
+
+    public String[] getStrings() {
+        return strings;
+    }
+
+    public void setStrings(String[] strings) {
+        this.strings = strings;
+    }
+}
